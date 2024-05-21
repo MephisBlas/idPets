d70feff6b1b8b5795e3e79a9c53462487e0d6abe
MephisBlas <127848314+MephisBlas@users.noreply.github.com>
2024-05-21 01:07:20 -0400

    first commit

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('Mascotas', views.Mascotas, name='Mascotas'),
+    path('registrar_mascotas', views.registrar_mascotas, name='registrar_mascotas'),
+    path('editar_mascotas', views.editar_mascotas, name='editar_mascotas'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+
+]
---
+++ app/views.py
@@ -0,0 +1,40 @@
+from sqlite3 import IntegrityError
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# Create your views here.
+
+def inicio(request):
+    return render (request, 'paginas/inicio.html')
+def nosotros(request):
+    return render (request,'paginas/nosotros.html')
+
+def Mascotas(request):
+    return render(request, 'Mascotas/index.html')
+
+def registrar_mascotas(request):
+    return render(request, 'Mascotas/registrar_mascotas.html')
+
+def editar_mascotas(request):
+    return render(request, 'Mascotas/editar_mascotas.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'paginas/signup.html', {"form": UserCreationForm})
+    else:
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+
+        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+def login(request):
+    return render (request,'paginas/login.html')
+
